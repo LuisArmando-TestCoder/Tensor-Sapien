5a631780fa510944757a4f4ece2b5fdfff449260
Luis Armando Murillo <luisarmando.murillobaltodano@gmail.com>
2022-07-18 22:42:59 -0600

    add canvas preset test for types hypergraph

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<canvas id="problemTypesHypergraph"></canvas>
+
+<script lang="TypeScript">
+    import { onMount } from 'svelte';
+    import preset from 'canvas-preset';
+  
+    onMount(() => {
+        const {
+            canvas,
+            draw,
+            render,
+            size
+        } = preset(null, "#problemTypesHypergraph");
+
+        size();
+        draw(() => {
+            render({
+                x: 0, y: 0, width: 100, height: 100, color: "#0f0"
+            }).rect();
+        });
+    });
+</script>
