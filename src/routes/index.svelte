ea21116c7fe2deb81a1a79740e9d302954cdb625
Luis Armando Murillo <luisarmando.murillobaltodano@gmail.com>
2022-07-21 02:12:51 -0600

    add connections connector idea

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,22 +1,43 @@
-<canvas id="problemTypesHypergraph"></canvas>
-
-<script lang="TypeScript">
-    import { onMount } from 'svelte';
-    import preset from 'canvas-preset';
-  
-    onMount(() => {
-        const {
-            canvas,
-            draw,
-            render,
-            size
-        } = preset(null, "#problemTypesHypergraph");
-
-        size();
-        draw(() => {
-            render({
-                x: 0, y: 0, width: 100, height: 100, color: "#0f0"
-            }).rect();
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import preset from 'canvas-preset';
+	let circles: { [index: string ]: { x: number, y: number, connections: string[] } } = {
+		a: { x: 300, y: 300, connections: ['c', 'b'] },
+		b: { x: 100, y: 100, connections: ['a', 'd'] },
+		c: { x: 100, y: 300, connections: ['d', 'b'] },
+		d: { x: 300, y: 100, connections: ['a', 'c'] }
+    };
+    const connections = Object.entries(circles).map(([key, { connections, x, y }]) => {
+        return connections.map(name => {
+            return {
+                thickness: 1,
+                color: "red",
+                group: [
+                    { x, y },
+                    { x: circles[name].x, y: circles[name].y }
+                ],
+                name: key
+            }
         });
-    });
+    }).flat()
+    const flattenedCircles = Object.values(circles);
+
+	onMount(() => {
+		const { c, draw, renderGroup, size } = preset(null, '#problemTypesHypergraph');
+
+		size();
+		draw(() => {
+            renderGroup('lines', connections);
+			renderGroup('arc', flattenedCircles, ({ x, y }: { x: number; y: number }) => {
+				return {
+					x,
+					y,
+					color: 'black',
+					radius: 50
+				};
+			});
+		});
+	});
 </script>
+
+<canvas id="problemTypesHypergraph" />
